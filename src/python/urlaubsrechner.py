e02fa76603455170ed88cdd49a288daa90932363
teicheld <teicheld@web.de>
2023-07-07 16:24:50 +0200

    Update urlaubsrechner.py
    mit fancy gluecksrad function

--- src/python/urlaubsrechner.py
+++ src/python/urlaubsrechner.py
@@ -1,3 +1,4 @@
+import time
 
 ''' age=alter
     disability=Behinderung
@@ -30,19 +31,19 @@ def check_holiday(age):
     else:
         holiday = 26
 
-    print(holiday)
+    #print(holiday)
     return holiday
 
 #################################################################################################################################################
 #                                                              Urlaubszusätze
 def check_boni(holiday, disability, age):
+    seniority = 0               # variable muss hier deklariert(erschaffen) werden, weil du die deklaration inerhalb der while schleife gemacht hast und du den wert auserhalb dieser abfragst, wo ihre gueltigkeit bereits beendet ist (dies passiert oefter in dem programm, aber python programmiert des um bei jeder ausfuehrung des programmes, damits trotzdem functioniert. siehe warnungen in thonny oder deiner ide)
     if disability >= 50:
         holiday +=5
 
     if age > 25:
         while True:
             try:
-                seniority = 0
                 seniority = int(input("Wie viel Jahre sind sie beriets im Betrieb "))
                 break
             except ValueError:
@@ -53,9 +54,26 @@ def check_boni(holiday, disability, age):
     return holiday
     
 #################################################################################################################################################
+#                                                       Fancy GluecksradAnzeige
+def fancyAnzeige(zahl):
+
+    for i in range(zahl):
+        print(i, end='')                        # end='' verhindert neue zeile zeichen
+        time.sleep(0.03)
+        if i/10 < 1:
+            print("\b", end='')                 # '\b' ist die taste ueber der enter taste (tippe "man ascii" in die websuchmaschine)
+        else:
+            print("\b\b", end='')
+    print(i, end='')            
+
+
+#################################################################################################################################################
+
 
 age, disability = get_inf()
 holiday = check_holiday(age)
 holiday = check_boni(holiday, disability, age)
 
-print("Sie haben",holiday ,"Tage an Urlaub")
+print("Sie haben ", end='')
+fancyAnzeige(holiday)
+print(" Tage an Urlaub")
